17a95cb44513da22bcb787d8173e98dfc25539dc
0WhyMe0 <wolodymyrpelekh@gmail.com>
2024-05-28 14:32:06 +0200

    Add project files.

---
+++ ZadWidDmytra/ActionsArrays.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadWidDmytra
+{
+    internal class ActionsArrays
+    {
+        public static double SumArray(params double [] array)
+        {
+            double result = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+
+            }
+            return result;
+        }
+
+        public static double MinArray(params double[] array)
+        {
+            double result = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < result)
+                    result = array[i];
+            }
+            return result;
+        }
+
+        public static double MaxArray(params double[] array)
+        {
+            double result = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= result)
+                    result = array[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ ZadWidDmytra/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+using ZadWidDmytra;
+
+class Program
+{
+    static void Main(string[] args)
+    {      
+
+        Console.WriteLine("Введвть величену масива: ");
+        int aelichenArray = int.Parse(Console.ReadLine());
+
+        double[] arrray = new double[aelichenArray];
+        double[] arrray2 = new double[aelichenArray];
+
+        for (int i = 0; i < arrray.Length; i++)
+        {
+            Console.WriteLine("Введіть числа в масив");
+            string str = Console.ReadLine();
+            bool ara = double.TryParse(str, out arrray[i]);
+            if (ara)
+            {
+                arrray2 = arrray;
+            }
+            else
+            {
+                Console.WriteLine("Введена не правильна іншормація");
+            }
+        }
+            double result = ActionsArrays.MaxArray(arrray2);
+        Console.WriteLine(result);
+
+        Console.WriteLine();
+    }
+}
